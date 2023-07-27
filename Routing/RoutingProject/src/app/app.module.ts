124dcb9376bc7116d41bf4b63e2721a03999824f
TemoElizbarashvili <115801621+TemoElizbarashvili@users.noreply.github.com>
2023-07-28 00:21:47 +0400

    add visual content and some components to Routing

--- Routing/RoutingProject/src/app/app.module.ts
+++ Routing/RoutingProject/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { ServersService } from './servers/servers.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    EditServerComponent,
+    ServerComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ ServersService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
