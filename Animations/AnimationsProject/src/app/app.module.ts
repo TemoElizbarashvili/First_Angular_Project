346d28e43b33af1c7f7db4762b7e10e1e56ac3cd
TemoElizbarashvili <115801621+TemoElizbarashvili@users.noreply.github.com>
2023-08-25 13:20:59 +0400

    Init Animations Project

--- Animations/AnimationsProject/src/app/app.module.ts
+++ Animations/AnimationsProject/src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { DetailsComponent } from './welcome/details/details.component';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WelcomeComponent,
-    DetailsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    SharedModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
